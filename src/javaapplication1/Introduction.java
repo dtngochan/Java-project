4b39f974585eecbb1d311ff7b340975465a89e60
dtngochan <dtngochan0604@gmail.com>
2024-10-29 00:33:34 +0700

    assignment 1

---
+++ src/javaapplication1/Introduction.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Ngoc Han
+ */
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Ho ten: Doan Thi Ngoc Han");
+        System.out.println("Lop: CMU-CS 303 AGIS");
+        System.out.println("Ma sinh vien: 29209049807");
+    }
+    
+}
